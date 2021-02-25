08bec93ef63c43b2de54234c2a5313fc59c200b6
Tumise <oladele.alade@gmail.com>
2021-02-26 00:10:19 +0100

    installed django heroku

--- conf/base.py
+++ conf/base.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -142,3 +142,6 @@ WAITER_INTERFACE_URL = env("WAITER_INTERFACE_URL")
 TWILIO_WHATSAPP_NUMBER = env("TWILIO_WHATSAPP_NUMBER")
 ACCOUNT_SID = env("ACCOUNT_SID")
 ACCOUNT_TOKEN = env("ACCOUNT_TOKEN")
+
+
+django_heroku.settings(locals())
