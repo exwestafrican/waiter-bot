d870f1c6cbe05ca4561cb0e638b5ea9d2bc7544e
Tumise <oladele.alade@gmail.com>
2021-02-25 23:57:49 +0100

    changed static file settings

--- conf/base.py
+++ conf/base.py
@@ -132,7 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 WAITER_INTERFACE_URL = env("WAITER_INTERFACE_URL")
 TWILIO_WHATSAPP_NUMBER = env("TWILIO_WHATSAPP_NUMBER")
