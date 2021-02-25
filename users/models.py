5a809d25fd5d4049d7eac02825e2bf9106ed6dfc
Tumise <oladele.alade@gmail.com>
2021-02-25 20:16:03 +0100

    added user  model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    phone = models.CharField(
+        max_length=15,
+        null=True,
+        help_text="user phone number e.g +23409050039030",
+        unique=True,
+    )
