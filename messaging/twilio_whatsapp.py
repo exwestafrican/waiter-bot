aca4dd047c9855608be74f5a9fa3fc390c0654aa
Tumise <oladele.alade@gmail.com>
2021-02-25 21:22:43 +0100

    added whatsapp setup

--- messaging/twilio_whatsapp.py
+++ messaging/twilio_whatsapp.py
@@ -7,11 +7,6 @@ auth_token = ACCOUNT_TOKEN
 
 client = Client(account_sid, auth_token)
 
-# message = client.messages.create(
-#     from_="whatsapp:+14155238886",
-#     body="Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/",
-#     to="whatsapp:+2348169084566",
-# )
 
 
 def send_whatsapp_message(to, from_, message):
