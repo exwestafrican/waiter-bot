322f9f69fe7001ab8eb7e2537ade5b6d9a571fde
Tumise <oladele.alade@gmail.com>
2021-02-26 12:23:29 +0100

    get channge implimentation of message_recived call back

--- messaging/webhook.py
+++ messaging/webhook.py
@@ -3,19 +3,21 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from messaging.twilio_whatsapp import send_whatsapp_message
+from twilio.twiml.messaging_response import MessagingResponse
 
 
 @api_view(["POST"])
 def message_received(request):
     if request.method == "POST":
         print("received message", request.data)
-        send_whatsapp_message(
-            "+2348169084566", "+14155238886", "got the message" + str(request.data)
-        )
-        return Response(
-            {"success": True, "message": "success", "data": "none"},
-            status=status.HTTP_200_OK,
-        )
+        # send_whatsapp_message(
+        #     "+2348169084566",
+        #     "+14155238886",
+        #     "got the message" + str(request.data.get("ProfileName")),
+        # )
+        response = MessagingResponse()
+        msg = response.message("Send us an image!")
+        return str(response)
 
 
 @api_view(["POST"])
