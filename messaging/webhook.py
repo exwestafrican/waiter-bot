75268b76e49b0533203365888c1cdc60cd124d59
Tumise <oladele.alade@gmail.com>
2021-02-26 02:03:02 +0100

    update message recived webhook

--- messaging/webhook.py
+++ messaging/webhook.py
@@ -9,6 +9,7 @@ from messaging.twilio_whatsapp import send_whatsapp_message
 def message_received(request):
     if request.method == "POST":
         print("received message", request.data)
+        send_whatsapp_message("+2348169084566", "+14155238886", "got the message")
         return Response(
             {"success": True, "message": "success", "data": "none"},
             status=status.HTTP_200_OK,
