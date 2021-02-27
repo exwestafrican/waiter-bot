f2e009d126e195187987121a8bb9387cf725a282
Tumise <oladele.alade@gmail.com>
2021-02-27 16:38:09 +0100

    added new app named commands

--- messaging/webhook.py
+++ messaging/webhook.py
@@ -2,11 +2,15 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from messaging.utils import clean_data, find_command
+
 from messaging.twilio_whatsapp import send_whatsapp_message
 from twilio.twiml.messaging_response import MessagingResponse
 
 from django.http import HttpResponse
+
+from commands.selectors import command_list
+
+from messaging.utils import clean_data, find_command
 
 
 @api_view(["POST"])
@@ -15,15 +19,21 @@ def message_received(request):
         msg = request.data.get("Body")
         sender = request.data.get("From")
         message = clean_data(msg)
+        response = MessagingResponse()
         command = find_command(msg)
+        if command:
+            handle_command()
+        else:
+            response.message(
+                "hey, {} we can't figure out what you want, but here are a list of commands: "
+            )
         # "#order rice-50,plantain-70,meat-50"
         # send_whatsapp_message(
         #     "+2348169084566",
         #     "+14155238886",
         #     "got the message" + str(request.data.get("ProfileName")),
         # )
-        response = MessagingResponse()
-        response.message("Send us an image!")
+
         return HttpResponse(str(response))
 
 
