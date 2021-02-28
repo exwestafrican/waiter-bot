dff2b657ed1bfba79b8bbaa973c1ef59159ab619
Tumise <oladele.alade@gmail.com>
2021-02-28 08:40:04 +0100

    added new app named locations and moved location items from users to new app

---
+++ location/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+# Create your models here.
+import uuid
+from django.db import models
+from utils.mixins import TimeStampMixin
+
+
+class Location(TimeStampMixin):
+    area = models.CharField(
+        max_length=800,
+        null=True,
+        help_text="locations are usually generic, like mainland or Ota",
+    )
+
+    state = models.CharField(
+        max_length=600,
+        help_text="lagos state",
+    )
+
+    popular_name = models.CharField(
+        max_length=800,
+        null=True,
+        help_text="where do people usually attribute this place to",
+    )
+
+    def __str__(self):
+        return "{},{}".format(self.area, self.state)
+
+    @property
+    def generic_address(self):
+        return "{} ({})".format(self.area, self.popular_name)
+
+    @property
+    def location_zip(self):
+        return self.pk
+
+
+class Restaurant(TimeStampMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(
+        max_length=500,
+        help_text="restaurant name",
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        help_text="who owns this restaurant",
+    )
+
+    address = models.OneToOneField(
+        Location,
+        help_text="where is your restaurant located",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    available_in = models.ManyToManyField(
+        Location,
+        related_name="available_in",
+        help_text="where are the places this restruant is located in",
+    )
+    code = models.CharField(max_length=4, null=True, blank=True, unique=True)
+    in_school = models.BooleanField(default=True)
