d6a101d91b68b05a58a6cb6ade21ee7d8c87322e
Tumise <oladele.alade@gmail.com>
2021-02-28 08:52:23 +0100

    added serializer for resturant model

--- location/serializers.py
+++ location/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from location.models import Location
+from location.models import Location, Restaurant
 
 
 class LocationModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = ["area", "state", "popular_name"]
+
+
+class RestaurantModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ["name", "owner", "address", "available_in", "code", "in_school"]
