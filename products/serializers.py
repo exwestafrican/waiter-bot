2d799722e36fad6c18887d990e3887ea0a590ef1
Tumise <oladele.alade@gmail.com>
2021-02-28 11:22:11 +0100

    bug fixed inheritance issue in product serializer

--- products/serializers.py
+++ products/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from products.models import Product
 
 
-class ProductModelSerializer(models.Model):
+class ProductModelSerializer(models.Serializer):
     class Meta:
         model = Product
         fields = ["name", "sold_by", "countable", "is_available", "measured_in"]
