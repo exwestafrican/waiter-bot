1e19ab9bd7ad57b0bc111f55238180c71e9b68e7
Tumise <oladele.alade@gmail.com>
2021-02-28 08:43:12 +0100

    removed location models in users app and change relationship to auth user model

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from utils.mixins import TimeStampMixin
-
+from django.conf import settings
 # Create your models here.
 
 
@@ -15,69 +15,9 @@ class User(AbstractUser):
         unique=True,
     )
 
-
-class Location(TimeStampMixin):
-    area = models.CharField(
-        max_length=800,
-        null=True,
-        help_text="locations are usually generic, like mainland or Ota",
-    )
-
-    state = models.CharField(
-        max_length=600,
-        help_text="lagos state",
-    )
-
-    popular_name = models.CharField(
-        max_length=800,
-        null=True,
-        help_text="where do people usually attribute this place to",
-    )
-
-    def __str__(self):
-        return "{},{}".format(self.area, self.state)
-
-    @property
-    def generic_address(self):
-        return "{} ({})".format(self.area, self.popular_name)
-
-    @property
-    def location_zip(self):
-        return self.pk
-
-
-class Restaurant(TimeStampMixin):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(
-        max_length=500,
-        help_text="restaurant name",
-    )
-    owner = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        help_text="who owns this restaurant",
-    )
-
-    address = models.OneToOneField(
-        Location,
-        help_text="where is your restaurant located",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-    )
-
-    available_in = models.ManyToManyField(
-        Location,
-        related_name="available_in",
-        help_text="where are the places this restruant is located in",
-    )
-    code = models.CharField(max_length=4, null=True, blank=True, unique=True)
-    in_school = models.BooleanField(default=True)
-
-
 class Profile(TimeStampMixin):
     user = models.OneToOneField(
-        User,
+        settings.AUTH_USER_MODEL,,
         primary_key=True,
         on_delete=models.CASCADE,
         help_text="is this measured in scopes, or boxes or bottles",
