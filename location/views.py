360d3f9be3443b7033ee8b3336171f900ba3b9e8
Tumise <oladele.alade@gmail.com>
2021-02-28 08:58:13 +0100

    added views for location model

--- location/views.py
+++ location/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
+
+from location.models import Location, Restaurant
+
+
+class LocationModelViewSet:
+    # add permission class to create
+    pass
