6a35b587683361c0b1d8bdb010a15d1a37d8ad2a
Tumise <oladele.alade@gmail.com>
2021-02-28 12:49:06 +0100

    ft admin func to create resturnat

---
+++ location/admin_api/services.py
@@ -0,0 +1,25 @@
+from location.models import Restaurant
+from location.utils import generate_restaurant_code
+from users.services import add_activity
+
+
+def create_restaurant(
+    admin,
+    name: str,
+    owner: object,
+    address: str,
+    available_in: object,
+    in_school: bool,
+):
+    restaurant = Restaurant(
+        name=name,
+        owner=user,
+        address=address,
+        available_in=available_in,
+        in_school=in_school,
+    )
+    location_zip = available_in.location_zip
+    code = generate_restaurant_code(location_zip)
+    restaurant.code
+    restaurant.save()
+    add_activity(admin, "{} created a new restaurant".format(admin))
--- users/utils.py
+++
@@ -1,7 +0,0 @@
-from string import ascii_letters, digits
-from random import choice
-
-
-def generate_restaurant_code(zip_code):
-    random_char = "".join([choice(ascii_letters + digits) for _ in range(3)])
-    return "{}{}".format(zip_code, random_char.lower())
