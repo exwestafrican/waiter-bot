4c2470c05397be2132418807fdcc7aa86d748a62
Tumise <oladele.alade@gmail.com>
2021-03-02 09:44:17 +0100

    removed cart relationship from cart items

--- cart/models.py
+++ cart/models.py
@@ -47,7 +47,7 @@ class Cart(TimeStampMixin):
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField(default=1)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    # cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
 
     def __str__(self):
         return "{} ({} {}}".format(
