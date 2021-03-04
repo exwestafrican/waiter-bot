13958c1a536a9821bdf4d558002c256f74016975
Tumise <oladele.alade@gmail.com>
2021-03-04 22:47:18 +0100

    store model serializer inherits from dynamic fields model serializer

--- location/serializers.py
+++ location/serializers.py
@@ -2,17 +2,16 @@ from rest_framework import serializers
 from location.models import Location, Store
 from utils.dynamic_serializers import DynamicFieldsModelSerializer
 
+
 class LocationModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = ["area", "state", "popular_name"]
 
 
-class StoreModelSerializer(serializers.ModelSerializer):
+class StoreModelSerializer(DynamicFieldsModelSerializer):
     available_in = LocationModelSerializer(read_only=True, many=True)
 
     class Meta:
         model = Store
         fields = ["id", "name", "owner", "address", "available_in", "code", "in_school"]
-
-    
